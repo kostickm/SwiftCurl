486e100bdb30c9bc67249f965aac20700ff1e6d2
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-27 20:48:44 -0600

    Using extern inline functions for wrapper shims
    IBM-Swift/Kitura#177

--- CurlHelpers.h
+++ CurlHelpers.h
@@ -22,16 +22,52 @@
 #define CURL_TRUE  1
 #define CURL_FALSE 0
 
-CURLcode curlHelperSetOptBool(CURL *curl, CURLoption option, int yesNo);
-CURLcode curlHelperSetOptHeaders(CURL *curl, struct curl_slist *headers);
-CURLcode curlHelperSetOptInt(CURL *curl, CURLoption option, long data);
-CURLcode curlHelperSetOptString(CURL *curl, CURLoption option, char *data);
-CURLcode curlHelperSetOptReadFunc(CURL *curl, void *userData, size_t (*read_cb) (char *buffer, size_t size, size_t nitems, void *userdata));
-CURLcode curlHelperSetOptWriteFunc(CURL *curl, void *userData, size_t (*write_cb) (char *ptr, size_t size, size_t nmemb, void *userdata));
+extern inline CURLcode curlHelperSetOptBool(CURL *curl, CURLoption option, int yesNo) {
+    return curl_easy_setopt(curl, option, yesNo == CURL_TRUE ? 1L : 0L);
+}
+
+extern inline CURLcode curlHelperSetOptHeaders(CURL *curl, struct curl_slist *headers) {
+    return curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+}
+
+extern inline CURLcode curlHelperSetOptInt(CURL *curl, CURLoption option, long data) {
+    return curl_easy_setopt(curl, option, data);
+}
+
+extern inline CURLcode curlHelperSetOptString(CURL *curl, CURLoption option, char *data) {
+    return curl_easy_setopt(curl, option, data);
+}
+
+extern inline CURLcode curlHelperSetOptReadFunc(CURL *curl, void *userData, size_t (*read_cb) (char *buffer, size_t size, size_t nitems, void *userdata)) {
+    
+    CURLcode rc = curl_easy_setopt(curl, CURLOPT_READDATA, userData);
+    if  (rc == CURLE_OK) {
+        rc = curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_cb);
+    }
+    return rc;
+    
+}
+
+extern inline CURLcode curlHelperSetOptWriteFunc(CURL *curl, void *userData, size_t (*write_cb) (char *ptr, size_t size, size_t nmemb, void *userdata)) {
+    
+    CURLcode rc = curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+    if  (rc == CURLE_OK)  {
+        rc = curl_easy_setopt(curl, CURLOPT_WRITEDATA, userData);
+        if  (rc == CURLE_OK) {
+            rc = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+        }
+    }
+    return rc;
+}
+
+extern inline CURLcode curlHelperGetInfoCString(CURL *curl, CURLINFO info, char **data) {
+    return curl_easy_getinfo(curl, info, data);
+}
 
 
-CURLcode curlHelperGetInfoCString(CURL *curl, CURLINFO info, char **data);
-CURLcode curlHelperGetInfoLong(CURL *curl, CURLINFO info, long *data);
+extern inline CURLcode curlHelperGetInfoLong(CURL *curl, CURLINFO info, long *data) {
+    return curl_easy_getinfo(curl, info, data);
+}
 
 
 #endif /* CurlHelpers_h */
